4a6ad37fe578008c0f24bcdf31cde2a19a134f10
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-07-31 15:35:43 -0600

    finished work and added comments

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-/*
+/* -=-=-=-=-=-=-=-=QUESTION1=-=-=-=-=-=-=-=-
 1. Write a function which takes an array of numbers and returns the mode of that array.
 [2, 4, 4, 4, 6, 6, 1] - > 4
 [0, 0, 0, 10] -> 0
 [1] -> 1
 */
+/* Solution Comments: This solution uses a combination of variables, for loops and if/else statements to output the mode of each array. Our first for loop/if else satement loops through the arrays and keeps track of how many times a number appears in the array. We then move ino the second part where we separate out the values and output the key value that appears the most in each array. */
+
 
 var mode = function (numbers) {
     var keepTrack = {};
@@ -29,12 +31,15 @@ var mode = function (numbers) {
 // console.log(mode([0, 0, 0, 10]));
 // console.log(mode([1]));
 
-/*
+/*-=-=-=-=-=-=-=-=QUESTION2=-=-=-=-=-=-=-=-
 2. Write a funciton which, given a year as a number, returns whether that number is a leap year.
 2000 -> true
 1900 -> false
 2016 -> true
 */
+/* Solution Comments: I used an if/else if/ else statement to find the solution to this problem. IF the year is divisible by 4 perfectly, AND NOT able to be divided by 100, then it is a leap year. ELSE IF the year is divisible by 4 AND 100 AND 400, then it is true, everything else is false. */
+
+
 
 var leapYear = function (year) {
     if (year % 4 === 0 && year % 100 !== 0) {
@@ -45,16 +50,18 @@ var leapYear = function (year) {
         return false;
     }
 };
-//
+
 // console.log(leapYear(2000));
 // console.log(leapYear(1900));
 // console.log(leapYear(2016));
 
-/* 3. Write a function which takes an array of integers > 0 and returns the first integer which does not appear in that array.
+/* -=-=-=-=-=-=-=-=QUESTION3=-=-=-=-=-=-=-=-
+3. Write a function which takes an array of integers > 0 and returns the first integer which does not appear in that array.
 [1,2,5] -> 3
 [1,2,3,4,5] -> 6
 [3,4,5] -> 1
 */
+/* Solution Comments: In this function, we loop through the array of numbers starting with 1 and if indexOf(i) is equal to -1 (meaning that it's not the next number) we return that index number with thes number that does not appear in the array.   */
 
 var firstInt = function (nums) {
     for (var i = 1; true; i++) {
@@ -70,17 +77,17 @@ var firstInt = function (nums) {
 
 
 
-/*
+/* -=-=-=-=-=-=-=-=QUESTION4=-=-=-=-=-=-=-=-
 4. Write a function which takes an array of integers and returns an array with any duplicate integers removed.
 [1,1,2,3,1,2,3] -> [1,2,3]
 [1,4,4,4,2,2,4,4,4] -> [1,4,2]
 */
-// My solution uses E6 Set functionallity.
-// Set takes an array and returns it without any duplicates.
-// I created my function in a variable called removeDuplicate
-// Within the function I created a new variable to hold the Set Method
-// within the set metod, I plug in my array from the function parentheses.
-// return newArr; and plug in the arrays.
+/* SOLUTION COMMENTS: My solution uses E6 Set functionallity.
+Set takes an array and returns it without any duplicates.
+I created my function in a variable called removeDuplicate
+Within the function I created a new variable to hold the Set Method
+within the set metod, I plug in my array from the function parentheses.
+return newArr; and plug in the arrays.*/
 
 var removeDuplicate = function (array) {
     var newArr = new Set(array);
@@ -91,7 +98,7 @@ var removeDuplicate = function (array) {
 // console.log(removeDuplicate([1,4,4,4,2,2,4,4,4]));
 
 
-/*
+/*-=-=-=-=-=-=-=-=QUESTION5=-=-=-=-=-=-=-=-
 5. Write function that translates a text to Pig Latin, and another that translates back. English is translated to Pig Latin by taking the first letter of every word, moving it to the end of the word and adding "ay".
 
 "The quick brown fox" -> "Hetay uickqay rownbay oxfay".
@@ -141,13 +148,14 @@ var pigEnglish = function (string) {
 // console.log(pigEnglish('Hetay uickqay rownbay oxfay'));
 
 
-/*
+/*-=-=-=-=-=-=-=-=QUESTION6=-=-=-=-=-=-=-=-
 6. Write a function which takes in two arrays and determines if they contain the same number of the same values.
 [], [] -> true
 [2, 3, 4], [1, 2, 3] -> false
 ["a", "c", "b"], ["a", "b", "c"] -> true
 [1, 1, 1], [1, 1, 1, 1] -> false
 */
+/* Solution Comments: This function takes two arrays and compares them to see if they are the same. I have two for loops that run the length of each respective array plugged in. I then run an IF/ELSE IF/ ELSE statment that 1. compares the length of each array and if they are not the same length, we return false. 2. if the index values of each array are the same, we return true, 3. everything else will return false.   */
 
 var samVar = function (arr, arr2) {
     for (i = 0; i <= arr.length; i++)
@@ -168,11 +176,12 @@ var samVar = function (arr, arr2) {
 // console.log(samVar([1, 1, 1], [1, 1, 1, 1]));
 
 
-/*
+/*-=-=-=-=-=-=-=-=QUESTION7=-=-=-=-=-=-=-=-
 7. Write a function which takes in an array of numbers and a max cutoff value, and returns a new array with elements limited by the cutoff value.
 [1,2,3,4,5,6,7,8], 4 -> [1,2,3,4,4,4,4,4]
 [1,5,7,3,1,5,7], 3 -> [1,3,3,3,1,3,3]
 */
+/* Solution Comments: For this solution, I have a function that takes the array and the desired cutoff value. I loop through the array and simply compare each index value of the arry with the number for the cutoff value, if that value is greater than the cutoff value, each index value after that in the arry becomes the cutoff value */
 
 var maxCut = function (arr, num) {
     for (var i = 0; i < arr.length; i++) {
@@ -186,7 +195,7 @@ var maxCut = function (arr, num) {
 // console.log(maxCut([1,5,7,3,1,5,7], 3));
 
 
-/*
+/*-=-=-=-=-=-=-=-=QUESTION8=-=-=-=-=-=-=-=-
 8. Write a function which takes no input and returns an array of 10 distinct randomly generated integers between 1 and 100.
 
 -> [48, 5, 32, 2, 10, 11, 34, 95, 82, 93] (good!)
@@ -210,12 +219,12 @@ var randomNumbers = function () {
     }
     return returnArray;
 };
-// returnArray.push();
-console.log(randomNumbers());
+
+// console.log(randomNumbers());
 
 
 
-/*
+/*-=-=-=-=-=-=-=-=QUESTION9=-=-=-=-=-=-=-=-
 9 Write a function which takes two sorted lists and merges them into a new sorted list.
 [1,2,5,6,9], [3,4,5,10] -> [1,2,3,4,5,5,6,9,10]
 [], [] -> []
@@ -247,6 +256,9 @@ var sorted = function(arr1, arr2) {
 // console.log(sorted([-1, 0, 1], [-2, 2]));
 
 
+/* ----------=-=-=-=-=-=-=-=-BONUS-=-=-=-=-=-=-=-=-----------------*/
+
+
 /*
 10. Write a function which, given an array, determines the subarray with the largest sum.
 [1, 1, 1, -1] -> [1, 1, 1]
@@ -258,22 +270,26 @@ var sorted = function(arr1, arr2) {
 // loop through length of array
 // determine which values will return a new array that would output largest sum
 //
-
+//     newArrMax = Math.max(arrMax, newArrMax);
+// } return newArrMax;
 // STILL A WORK IN PROGRESS
 
 var subArray = function (arr) {
-    var arrMax = 0;
-    var newArrMax = 0;
+    var arrMax = [];
+    var newArrMax = [];
     for (var i = 0; i < arr.length; i++) {
-        arrMax = Math.max(0, arrMax + arr[i]);
-        newArrMax = Math.max(arrMax, newArrMax);
-    } return newArrMax;
+        arrMax = Math.max(0, arrMax, arr[i]);
+        newArr = arrMax.push(arr[i]);
+        newArrMax = Math.max(arrMax.push(), newArrMax);
+        // console.log(nnewArr);
+ }
 };
 
-console.log(subArray([1, 1, 1, -1]));
-console.log(subArray([1, 5, -4, 3, 2, -3]));
-console.log(subArray([2, 2, -10, 5, -10, 2, 2]));
-console.log(subArray([1, 3, -4, 5, 7, 9]));
+
+// console.log(subArray([1, 1, 1, -1]));
+// console.log(subArray([1, 5, -4, 3, 2, -3]));
+// console.log(subArray([2, 2, -10, 5, -10, 2, 2]));
+// console.log(subArray([1, 3, -4, 5, 7, 9]));
 
 /*
 11. One way of encoding a message is called a "square code". Given a message like:
@@ -287,14 +303,38 @@ We then read the square in columns, which gives us our encoded message:
 hae! and via ecy
 Write two functions, called encodeSquare and decodeSquare. encodeSquare should be able to encrypt any arbitrary string, and decodeSquare should be able to unencrypt any arbitrary encoded string. The unencrypted string will have no spaces, and that's ok.
 */
+// STILL A WORK IN PROGRESS
+
+var encodeSquare = function (input) {
+    var inputAsArray = input.split('');
+    inputAsArray.forEach(function(arr){
+        arr = arr.split('');
+    });
+    var rootOf = Math.sqrt(inputAsArray.length);
+    if (rootOf === Math.floor(rootOf)) {
+        rootOf++
+    }
+    var output = [];
+    for (var i = 0; i < rootOf; i++){
+        output.push([]);
+    }
+    for (var i = 0; i < output.length; i++) {
+        for (var j = 0; j < rootOf; j++) {
+            output[i].push(inputAsArray.shift());
+        }
+    }
+    for (var i = 0; i < output.length; i++){
+        output[i] = output[i].join('');
+    }
+    return output.join('\n');
+};
+// console.log(encodeSquare("Have a nice day!"));
 
 
 
-
-
-
-
-
+// var unSquare = function (input) {
+//     return input.split('\n').join('')
+// };
 
 /*
 12. Write a program to count the occurrences of all letter pairs in a sample of text (like the first paragraph of the Constitution). Disregard differences between lower and upper case letters. Output the 10 most frequent letter pairs, in order by percent of total. Your program should correctly process situations where the input string is empty or where less than 10 pairs occur.
